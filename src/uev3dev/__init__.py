e6d174901fdf90d0c66f34dd47f622f054ddf6b0
David Lechner <david@lechnology.com>
2017-09-22 16:01:52 -0500

    Let's not encourage people to import *

--- src/uev3dev/__init__.py
+++ src/uev3dev/__init__.py
@@ -2,13 +2,3 @@
 
 Write some stuff here.
 """
-
-from uev3dev.display import Display
-from uev3dev.motors import *
-from uev3dev.sensors import *
-from uev3dev.buttons import Buttons
-from uev3dev.leds import StatusLight
-from uev3dev.ports import *
-from uev3dev.util import debug_print
-from uev3dev.util import fork
-from uev3dev.i2c import SMBus
